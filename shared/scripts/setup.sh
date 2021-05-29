257f6f9237da529e61930f72e82b1c990e8234be
Joaquín Bermúdez Castanheira <jbermudez@thecloudsaviour.com>
2021-05-29 20:41:32 +0200

    fix: add swap to AMI

--- shared/scripts/setup.sh
+++ shared/scripts/setup.sh
@@ -5,6 +5,13 @@ set -e
 # Disable interactive apt prompts
 export DEBIAN_FRONTEND=noninteractive
 
+# Allocate, enable and add swap to fstab
+sudo fallocate -l 4G /swap
+sudo chmod 600 /swap
+sudo mkswap /swap
+sudo swapon /swap
+echo -e "/swap swap swap defaults 0 0" | sudo tee -a /etc/fstab
+
 cd /ops
 
 CONFIGDIR=/ops/shared/config
