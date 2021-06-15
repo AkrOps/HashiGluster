ba06518c13bd63e5664cd73850128d472c063cb4
Joaquín Bermúdez Castanheira <jbermudez@thecloudsaviour.com>
2021-06-15 19:11:53 +0200

    feat: add script to enable gossip encryption in all Consul agents

---
+++ utils/scripts/gossip-encrypt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This scripts enables Gossip encryption for all Consul agents.
+# It should be run from inside the Terraform environment.
+
+shopt -s expand_aliases
+CONFIG_FILE="/etc/consul.d/consul.hcl"
+alias ssh="ssh -l ubuntu -o StrictHostKeyChecking=accept-new"
+
+declare -a HOST_IPs=()
+
+for i in $(terraform output | grep 'public IPs' | grep -oP '(\d+\.){3}\d+'); do HOST_IPs+=($i); done
+
+# Grab encryption key if it exists (cat to local sed to reduce complexity)
+KEY=$(ssh ${HOST_IPs[0]} cat $CONFIG_FILE | sed -rn 's/encrypt = "(.+)"/\1/p')
+
+# Otherwise, generate a new key
+if ! [ $KEY ]; then
+	KEY=$(ssh ${HOST_IPs[0]} consul keygen)
+fi
+
+# Add encryption key to consul agent config
+for i in ${HOST_IPs[@]}; do
+	if ! [ "$(ssh $i grep -o 'encrypt' $CONFIG_FILE)" ]; then ssh $i "printf '\n\nencrypt = \"$KEY\"\n' | sudo tee -a $CONFIG_FILE"; fi
+done
+
+# Restart Consul service
+for i in ${HOST_IPs[@]}; do ssh $i 'sudo systemctl restart consul'; done
